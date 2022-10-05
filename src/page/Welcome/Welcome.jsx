3644ffae82529fba01a9c7a59947f2877e4f5ee8
Zheronimo <kirichenko.e.o@gmail.com>
2022-10-05 20:44:33 +0300

    блок welkom добавлен 3d поворот

--- src/page/Welcome/Welcome.jsx
+++ src/page/Welcome/Welcome.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Btn from '../../componets/btn/Btn';
 import Socials from '../../componets/socials/Socials';
-
 import User from '../../componets/user/User';
 
 import './welcome.css';
 
 const Welcome = () => {
+  const [rotateUser, setRotateUser] = useState('welcome__user');
+  const [rotateAutor, setRotateAutor] = useState('welcome__autor');
+
+  const flipCart = (e) => {
+    e.preventDefault();
+    rotateUser === 'welcome__user'
+      ? setRotateUser('welcome__user welcome__user--rotate')
+      : setRotateUser('welcome__user');
+    rotateAutor === 'welcome__autor'
+      ? setRotateAutor('welcome__autor welcome__autor--rotate')
+      : setRotateAutor('welcome__autor');
+  };
+
   return (
     <div className='welcome'>
-      <a href='/' className='welcome__link'>
+      {/* <a href='/' className='welcome__link'>
         Авторизироваться
-      </a>
+      </a> */}
+      <Btn
+        link='/'
+        text='Авторизоваться'
+        className='btn btn--radius welcome__link'
+        onClick={flipCart}
+      />
       <div className='welcom__content'>
-        <div className='welcome__user'>
+        <div className={rotateUser}>
           <User />
           <Socials />
           <div className='welcome__btn'>
@@ -21,6 +39,9 @@ const Welcome = () => {
             <Btn link='/' text='Обо мне' className='btn btn--border' />
             <Btn link='/' text='Блог' className='btn' />
           </div>
+        </div>
+        <div className={rotateAutor}>
+          <h1>АВтор</h1>
         </div>
       </div>
       <footer className='welcome__footer'>
